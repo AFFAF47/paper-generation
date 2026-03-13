fb652fcbc746c69a05681f7ee03ab0fe2d50a34e
AFFAF47 <anasaffaf@gmail.com>
2026-03-14 03:57:15 +0530

    initial commit

---
+++ src/main/java/com/example/exam/papergenerator/PapergeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam.papergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapergeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapergeneratorApplication.class, args);
+	}
+
+}
